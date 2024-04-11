f167ce58dcf30537bcff4fe862f32cf16e5e8d1c
Himanshu Sharma <Himanshupandit9350373965@gmail.com>
2024-04-11 10:17:53 +0530

    All Changes Done

--- force-app/main/default/lwc/metaDataComponentChange/metaDataComponentChange.js
+++ force-app/main/default/lwc/metaDataComponentChange/metaDataComponentChange.js
@@ -28,16 +28,19 @@ import Please_input_a_start_date_that_is_earlier_than_today_s_date from '@salesf
 import Please_specify_the_type_of_data_retrieval from '@salesforce/label/c.Please_specify_the_type_of_data_retrieval';
 import XmlStartLabel from '@salesforce/label/c.xmlStartLabel';
 import XmlEndLabel from '@salesforce/label/c.xmlEndLabel';
-
+import Warning from '@salesforce/label/c.Warning';
+import HardRefresh from '@salesforce/label/c.HardRefresh';
+import No_metadata_found_between_start_date_and_end_date from '@salesforce/label/c.No_metadata_found_between_start_date_and_end_date';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class MetaDataComponentChange extends LightningElement {
     retriveData = [];
-    isLoading = false;
+    maindata = [];
+    isLoading = true;
     showFirstScreen = true;
     showSecondScreen = false;
     metaDataComponent = Meta_Morphing_Monitor;
     startDate;
-    endDate;XmlEndLabel
+    endDate;
     selectedType;
     xmlFileData;
     csvData;
@@ -49,9 +52,19 @@ export default class MetaDataComponentChange extends LightningElement {
     labelDownlaod = Download;
     labelCopy = Copy;
     labelRadioButton = Please_specify_the_type_of_data_retrieval;
+    labelRefresh = HardRefresh;
+    isError = false;
+    errorMessage = '';
     get options() {
         return [{ label: CSV, value: CSV },
         { label: XML, value: XML }]
+    }
+    connectedCallback() {
+        try {
+            this.callBackend();
+        } catch (error) {
+            console.error(error);
+        }
     }
     handleChange(event) {
         try {
@@ -68,39 +81,72 @@ export default class MetaDataComponentChange extends LightningElement {
     }
     handleClick(event) {
         try {
-            if(event.target.label == Submit){
+            if (event.target.label == Submit) {
                 this.isLoading = true;
                 let returnData = this.validateData();
                 if (returnData) {
-                    this.callBackend();
+                    this.configData();
                 } else {
                     this.isLoading = false;
                 }
             }
-            if(event.target.label == Back){
+            if (event.target.label == Back) {
                 this.showFirstScreen = true;
                 this.showSecondScreen = false;
                 this.data = [];
                 this.labelTextArea = '';
                 this.labelButton = Submit
             }
-            if(event.target.label == Download){
+            if (event.target.label == Download) {
                 const hiddenElement = document.createElement('a');
                 hiddenElement.href = this.selectedType == XML ? Application_xml + encodeURI(this.xmlFileData) : Text_csv + encodeURI(this.csvData);
                 hiddenElement.target = '_blank';
-                hiddenElement.download = this.selectedType == XML ?Data_xml:Data_csv;
+                hiddenElement.download = this.selectedType == XML ? Data_xml : Data_csv;
                 hiddenElement.click();
             }
-            if(event.target.label == Copy){
+            if (event.target.label == Copy) {
                 const textarea = document.createElement('textarea');
                 textarea.value = this.data;
                 document.body.appendChild(textarea);
                 textarea.select();
                 document.execCommand('copy');
                 document.body.removeChild(textarea);
-                this.showMessage(Data_copied,Success,Success);
+                this.showMessage(Data_copied, Success, Success);
+            }
+            if(event.target.label == this.labelRefresh){
+                this.isLoading = true;
+                this.callBackend();
             }
 
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    configData() {
+        try {
+            let completeData = [];
+            this.maindata.forEach(currentItem => {
+                if (currentItem.lastModifiedDate >= this.startDate && currentItem.lastModifiedDate <= this.endDate) {
+                    completeData.push({
+                        compName: currentItem.compName,
+                        compType: currentItem.compType,
+                        lastModifiedDate: currentItem.lastModifiedDate
+                    });
+                }
+            });
+            this.retriveData = this.removeDuplicates(completeData);
+            if (this.retriveData.length == 0) {
+                this.showMessage(No_metadata_found_between_start_date_and_end_date,Warning,Warning);
+            } else {
+                this.createXmlData();
+                this.createCSVData();
+                this.data = this.selectedType == XML ? this.xmlFileData : this.csvData;
+                this.labelTextArea = this.selectedType == XML ? XML_Data : CSV_Data;
+                this.showFirstScreen = false;
+                this.showSecondScreen = true;
+                this.labelButton = Back;
+            }
+            this.isLoading = false;
         } catch (error) {
             console.error(error);
         }
@@ -109,30 +155,15 @@ export default class MetaDataComponentChange extends LightningElement {
         try {
             getAllData().then((result) => {
                 if (result.isSuccess) {
-                    let completeData = [];
-                    let data = JSON.parse(JSON.stringify(result.metaData));
-                    data.forEach(currentItem => {
-                        if (currentItem.lastModifiedDate >= this.startDate && currentItem.lastModifiedDate <= this.endDate) {
-                            completeData.push({
-                                compName: currentItem.compName,
-                                compType: currentItem.compType,
-                                lastModifiedDate: currentItem.lastModifiedDate
-                            });
-                        }
-                    });
-                    this.retriveData = this.removeDuplicates(completeData);
-                    this.createXmlData();
-                    this.createCSVData();
-                    this.data = this.selectedType == XML ? this.xmlFileData : this.csvData;
-                    this.labelTextArea = this.selectedType == XML ? XML_Data : CSV_Data;
-                    this.showFirstScreen = false;
-                    this.showSecondScreen = true;
-                    this.labelButton = Back;
+                    this.maindata = JSON.parse(JSON.stringify(result.metaData));
                 } else {
-                    this.showMessage(result.message,Error,Error);
+                    this.showFirstScreen = false;
+                    this.isError = true;
+                    this.errorMessage = result.message;
                 }
                 this.isLoading = false;
             }).catch((err) => {
+                this.showMessage(err,Error,Error);
                 console.error(err);
             });
         } catch (error) {
@@ -166,7 +197,7 @@ export default class MetaDataComponentChange extends LightningElement {
                 }
                 types[item.compType].push(item.compName);
             });
-            let xml = XmlStartLabel+'\n';
+            let xml = XmlStartLabel + '\n';
             for (let type in types) {
                 xml += '\t<types>\n';
                 types[type].forEach(member => {
@@ -175,7 +206,7 @@ export default class MetaDataComponentChange extends LightningElement {
                 xml += `\t\t<name>${type}</name>\n`;
                 xml += '\t</types>\n';
             }
-            xml += '\t'+XmlEndLabel+'\n</Package>';
+            xml += '\t' + XmlEndLabel + '\n</Package>';
             this.xmlFileData = xml;
         } catch (error) {
             console.error(error);
@@ -184,22 +215,22 @@ export default class MetaDataComponentChange extends LightningElement {
     validateData() {
         try {
             if (this.startDate == undefined || this.startDate == '') {
-                this.showMessage(Kindly_provide_the_start_date, Error,Error);
+                this.showMessage(Kindly_provide_the_start_date, Error, Error);
                 return false;
             } else if (new Date(this.startDate) > new Date()) {
-                this.showMessage(Please_input_a_start_date_that_is_earlier_than_today_s_date, Error,Error);
+                this.showMessage(Please_input_a_start_date_that_is_earlier_than_today_s_date, Error, Error);
                 return false;
             } else if (this.endDate == undefined || this.endDate == '') {
-                this.showMessage(Kindly_input_the_end_date,Error,Error);
+                this.showMessage(Kindly_input_the_end_date, Error, Error);
                 return false;
             } else if (new Date(this.endDate) > new Date()) {
-                this.showMessage(Please_input_a_end_date_that_is_earlier_than_today_s_date, Error,Error);
+                this.showMessage(Please_input_a_end_date_that_is_earlier_than_today_s_date, Error, Error);
                 return false;
             } else if (new Date(this.endDate) < new Date(this.startDate)) {
-                this.showMessage(The_end_date_must_be_greater_than_or_equal_to_the_start_date, Error,Error);
+                this.showMessage(The_end_date_must_be_greater_than_or_equal_to_the_start_date, Error, Error);
                 return false;
             } else if (this.selectedType == undefined) {
-                this.showMessage(Please_select_Retrieve_Data,Error,Error);
+                this.showMessage(Please_select_Retrieve_Data, Error, Error);
                 return false;
             } else {
                 return true;
